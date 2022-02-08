b3cd7d9070de7328c4cb95fe4f9ce4cc23c2558f
João Pedro Soares <joao.p.soares93@gmail.com>
2022-02-08 23:39:35 +0000

    Project folder setup

---
+++ MovieExplorer/Application/Scenes/Browse/BrowseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  BrowseCoordinator.swift
+//  MovieExplorer
+//
+//  Created by João Pedro on 08/02/2022.
+//
+
+import Foundation
---
+++ MovieExplorer/Application/Scenes/Details/DetailsCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsCoordinator.swift
+//  MovieExplorer
+//
+//  Created by João Pedro on 08/02/2022.
+//
+
+import Foundation
