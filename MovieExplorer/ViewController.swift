703eadcd4100651c2f7ec96187858f26783341ea
João Pedro Soares <joao.p.soares93@gmail.com>
2022-02-08 01:16:24 +0000

    Initial project setup

---
+++ MovieExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieExplorer
+//
+//  Created by João Pedro on 08/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
